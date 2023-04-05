8ec9ed172e75d810513f611688ad28acae52b1ba
Narek <narek.avagyan.tw@gmail.com>
2023-04-05 22:44:52 +0400

    added hero and header

---
+++ src/components/svgs/StarIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StarIcon = () => {
+  return (
+    <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0 13C8.05634 11.7183 11.9014 8.42253 13 0C13.9155 8.42253 18.3099 11.7183 26 13C18.3099 14.2817 14.2817 18.3099 13 26C11.7183 18.3099 8.05634 14.2817 0 13Z"
+        fill="#3D65F7"
+      />
+    </svg>
+  );
+};
+
+export default StarIcon;
