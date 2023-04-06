bb8a399e3992c4218ac1127b1e45e17207e8f69c
4tbf <4tbfcompanydev@gmail.com>
2023-04-06 14:17:16 +0300

    Actual dev

---
+++ src/components/svgs/ArrowIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ArrowIcon = () => {
+  return (
+    <svg width="10" height="5" viewBox="0 0 10 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 0.5L5 3.5L9 0.5" stroke="white" />
+    </svg>
+  );
+};
+
+export default ArrowIcon;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
-import styles from '../styles/Home.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import Header from '../components/header/Header';
+import styles from './index.module.scss';
+import LongArrowIcon from '../components/svgs/LongArrowIcon';
 
 export default function Home() {
   return (
@@ -11,8 +14,115 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Home page DEV
-      <main className={styles.main} />
+      <Header />
+      <main className={styles.main}>
+        <section
+          className={styles.heroSection}
+          style={{ backgroundImage: 'url(/images/brand_image.png)' }}
+        >
+          <div className="container">
+            <div className={styles.heroRow}>
+              <div className="col_">
+                <p className={styles.subTitle}>
+                  Hit a line and our friendly folks will get back to you as soon as possible.
+                </p>
+                <h1>Stex Mi Hat Bomb Title Mrrr</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className={styles.scrolledSection}>
+          <div className={styles.scroledItems}>
+            <div className={styles.scroledItem}>
+              <p>Quality assurance</p>
+            </div>
+            <div className={styles.scroledItem}>
+              <p>Safety</p>
+            </div>
+            <div className={styles.scroledItem}>
+              <p>modernity</p>
+            </div>
+            <div className={styles.scroledItem}>
+              <p>rapidity</p>
+            </div>
+            <div className={styles.scroledItem}>
+              <p>adaptability</p>
+            </div>
+            <div className={styles.scroledItem}>
+              <p>uniqueness</p>
+            </div>
+          </div>
+        </section>
+        <section className={styles.ourServices}>
+          <div className="container">
+            <div className={styles.servicesRow}>
+              <div className="col_">
+                <div className={styles.mainItem}>
+                  <h2 className={styles.mainTitle}>OUR SERVICES</h2>
+                  <Link href="#" className={styles.arrowLink}>
+                    Show more <LongArrowIcon />
+                  </Link>
+                </div>
+              </div>
+              <div className="col_">
+                <Link href="#" className={styles.clasicItem}>
+                  <div className={styles.imageItem}>
+                    <Image src="/images/brand_image.png" height={66} width={72} alt="image" />
+                  </div>
+                  <h2 className={styles.serviceTitle}>Web Development</h2>
+                  <p className={styles.serviceDescr}>
+                    Hit a line and our friendly folks will get back to you as soon as possible.
+                  </p>
+                </Link>
+              </div>
+              <div className="col_">
+                <Link href="#" className={styles.clasicItem}>
+                  <div className={styles.imageItem}>
+                    <Image src="/images/brand_image.png" height={66} width={72} alt="image" />
+                  </div>
+                  <h2 className={styles.serviceTitle}>Web Development</h2>
+                  <p className={styles.serviceDescr}>
+                    Hit a line and our friendly folks will get back to you as soon as possible.
+                  </p>
+                </Link>
+              </div>
+              <div className="col_">
+                <Link href="#" className={styles.clasicItem}>
+                  <div className={styles.imageItem}>
+                    <Image src="/images/brand_image.png" height={66} width={72} alt="image" />
+                  </div>
+                  <h2 className={styles.serviceTitle}>Web Development</h2>
+                  <p className={styles.serviceDescr}>
+                    Hit a line and our friendly folks will get back to you as soon as possible.
+                  </p>
+                </Link>
+              </div>
+              <div className="col_">
+                <Link href="#" className={styles.clasicItem}>
+                  <div className={styles.imageItem}>
+                    <Image src="/images/brand_image.png" height={66} width={72} alt="image" />
+                  </div>
+                  <h2 className={styles.serviceTitle}>Web Development</h2>
+                  <p className={styles.serviceDescr}>
+                    Hit a line and our friendly folks will get back to you as soon as possible.
+                  </p>
+                </Link>
+              </div>
+              <div className="col_">
+                <Link href="#" className={styles.clasicItem}>
+                  <div className={styles.imageItem}>
+                    <Image src="/images/brand_image.png" height={66} width={72} alt="image" />
+                  </div>
+                  <h2 className={styles.serviceTitle}>Web Development</h2>
+                  <p className={styles.serviceDescr}>
+                    Hit a line and our friendly folks will get back to you as soon as possible.
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
