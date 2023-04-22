a70ee19e18be2382b92d99c7fd886a8b8286481a
4tbf <4tbfcompanydev@gmail.com>
2023-04-22 16:19:53 +0300

    about texts

--- src/features/molecules/about/about-hero/AboutHero.jsx
+++ src/features/molecules/about/about-hero/AboutHero.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { useTranslation } from 'next-i18next';
 import styles from './AboutHero.module.scss';
 import Text from '../../../atoms/text';
 import Button from '../../../../components/multiusable/button/Button';
+import SuccessModal from '../../../atoms/SuccessModal';
 
 const AboutHero = () => {
+  const { t } = useTranslation();
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
   return (
     <section className={styles.aboutHero}>
+      <SuccessModal setIsOpen={setIsOpen} modalIsOpen={modalIsOpen} />
+
       <div className="container">
         <Text as="h1" className={styles.aboutTitle}>
-          About Us bayc karelia yerkar .
+          {t('about.subtitle')}
         </Text>
         <Text as="p" className={styles.aboutSubTitle}>
-          Lorem ipsum dolor sit amet consectetur. Dolor ultrices amet
+          {t('about.title')}
         </Text>
-        <Button type="button" children="Work With us" variant="light" />
+        <Button
+          type="button"
+          children={t('about.workwithus')}
+          onClick={() => setIsOpen(true)}
+          variant="light"
+        />
       </div>
     </section>
   );
