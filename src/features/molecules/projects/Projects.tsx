4eb90bb4d32dd34a63741d89c5c21f4bf7a1ee12
Narek <narek.avagyan.tw@gmail.com>
2023-04-23 00:07:39 +0400

    home services section

--- src/features/molecules/projects/Projects.tsx
+++ src/features/molecules/projects/Projects.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import Image from 'next/image';
+import clsx from 'clsx';
 import styles from './Projects.module.scss';
 import Text from '../../atoms/text';
 import Button from '../../../components/multiusable/button/Button';
+import { useMediaQuery } from '../../../hooks/useMediaQuery';
 
 const Projects = () => {
+  const tablet = useMediaQuery('(max-width: 1279.98px)');
+  const mobile = useMediaQuery('(max-width: 767.98px)');
   return (
     <section className={styles.projectsSection}>
       <div className={styles.projectsContainer}>
         <div className={styles.projectsRow}>
           <div className="col_">
             <div className={styles.projecstsImage}>
-              <Image alt="image" src="/images/home-projects/nfc-image.pnf" />
+              <Image
+                width={1447}
+                height={1400}
+                alt="image"
+                src={
+                  tablet
+                    ? '/images/home-projects/tablet_nfc-image.png'
+                    : '/images/home-projects/nfc-image.png'
+                }
+              />
             </div>
           </div>
+
           <div className="col_">
             <div className={styles.aboutProjectInfo}>
               <Text as="p" className={styles.projectName}>
@@ -30,6 +44,24 @@ const Projects = () => {
             </div>
           </div>
         </div>
+        {!mobile && (
+          <div className={styles.projectsDots}>
+            <button
+              aria-label="dot"
+              type="button"
+              className={clsx(styles.dotsItem, {
+                [styles.activeDots]: true,
+              })}
+            />
+            <button
+              aria-label="dot"
+              type="button"
+              className={clsx(styles.dotsItem, {
+                [styles.activeDots]: false,
+              })}
+            />
+          </div>
+        )}
       </div>
     </section>
   );
