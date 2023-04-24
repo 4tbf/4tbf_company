f4c26de8df21f0778e6d8090a29cba3c9411639e
Narek <narek.avagyan.tw@gmail.com>
2023-04-24 21:44:58 +0400

    some styles

--- src/features/molecules/case-studies-components/case-study-items/CaseStudyIems.tsx
+++ src/features/molecules/case-studies-components/case-study-items/CaseStudyIems.tsx
@@ -49,7 +49,49 @@ const CaseStudyIems = () => {
               <div className={styles.innerRow}>
                 <div className="col_">
                   <div className={styles.innerContent}>
-                    <div className={styles.imageWrapper}>{/* IMAGE */}</div>
+                    <div className={styles.imageWrapper}>
+                      <Image
+                        alt="case study"
+                        src="/images/case-studies/tonus.png"
+                        width={576}
+                        height={269}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col_">
+                  <div className={styles.itemContent}>
+                    <Text as="h2">Some title</Text>
+                    <div className={styles.itemMarks}>
+                      <Text as="p">PHP</Text>
+                      <Text as="p">Laravel</Text>
+                      <Text as="p">JavaScript</Text>
+                    </div>
+                    <Text as="p" className={styles.itemDescr}>
+                      Lorem ipsum dolor sit amet consectetur. Fusce neque pharetra suspendisse sed
+                      ullamcorper imperdiet aliquet. Justo lobortis nam nulla ac enim amet. Arcu
+                      nulla platea quis viverra integer quam integer id diam. Nulla urna elit id
+                      nullam. Eget odio molestie quam leo consequat suscipit ut elementum aliquam.
+                    </Text>
+                    <ArrowButton text="Show more" href="#" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col_">
+            <div className={styles.item}>
+              <div className={styles.innerRow}>
+                <div className="col_">
+                  <div className={styles.innerContent}>
+                    <div className={styles.imageWrapper}>
+                      <Image
+                        alt="case study"
+                        src="/images/case-studies/qr.png"
+                        width={576}
+                        height={269}
+                      />
+                    </div>
                   </div>
                 </div>
                 <div className="col_">
