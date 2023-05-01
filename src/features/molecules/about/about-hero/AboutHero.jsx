6e60e9f844bb7df602505ecf6fb29cac4f98988b
4tbf <4tbfcompanydev@gmail.com>
2023-05-01 12:35:21 +0300

    Bugfixes 01.05.2023

--- src/features/molecules/about/about-hero/AboutHero.jsx
+++ src/features/molecules/about/about-hero/AboutHero.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
+import { motion, AnimatePresence } from 'framer-motion';
 import styles from './AboutHero.module.scss';
-import Text from '../../../atoms/text';
 import Button from '../../../../components/multiusable/button/Button';
 
 const AboutHero = () => {
@@ -12,12 +12,26 @@ const AboutHero = () => {
   return (
     <section className={styles.aboutHero}>
       <div className="container">
-        <Text as="h1" className={styles.aboutTitle}>
-          {t('about.subtitle')}
-        </Text>
-        <Text as="p" className={styles.aboutSubTitle}>
-          {t('about.title')}
-        </Text>
+        <AnimatePresence>
+          <motion.h1
+            className={styles.aboutTitle}
+            initial={{ x: -1000, scale: 0.7 }}
+            animate={{ x: 0, scale: 1 }}
+            transition={{ duration: 0.8 }}
+            key={t('about.subtitle')}
+          >
+            {t('about.subtitle')}
+          </motion.h1>
+          <motion.p
+            className={styles.aboutSubTitle}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            key={t('about.title')}
+          >
+            {t('about.title')}
+          </motion.p>
+        </AnimatePresence>
         <Button
           type="button"
           children={t('about.workwithus')}
